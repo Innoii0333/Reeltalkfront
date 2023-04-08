abf5ca135b0fa492d0a8092e11105958ee14336e
Innoii0333 <hin2520@gmail.com>
2023-04-08 17:28:47 +0900

    로그인

---
+++ src/pages/login/sso.vue
@@ -0,0 +1,29 @@
+<script setup>
+import axios from 'axios'
+
+const router = useRouter()
+
+const session = ref(null)
+onMounted(() => {
+  axios.get('/api/session')
+    .then((res) => {
+      const userToken = res.json().token_id
+      useSessionStore().setToken(userToken)
+      router.push('/main')
+    })
+    .catch((e) => {
+      console.error(e)
+      router.push('/login')
+    })
+})
+</script>
+
+<template>
+  <div>
+    <p>
+      "checking the session..."
+    </p>
+  </div>
+</template>
+
+<style></style>
