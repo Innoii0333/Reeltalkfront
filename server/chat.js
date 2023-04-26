cdab3f02286df15ed4439705a09fcc634a15059a
Lee <lsangm0513@gmail.com>
2023-04-26 16:29:45 +0900

    오픈채팅 추가

--- server/chat.js
+++ server/chat.js
@@ -1,7 +1,7 @@
 import { WebSocketServer } from 'ws'
 
 // server.js
-const server = new WebSocketServer({ port: 8080 })
+const server = new WebSocketServer({ port: 8079 })
 
 server.on('connection', (socket) => {
   console.log('클라이언트가 연결되었습니다.')
@@ -9,12 +9,20 @@ server.on('connection', (socket) => {
   // 클라이언트로부터 메시지를 받았을 때의 이벤트 리스너
   socket.on('message', (message) => {
     console.log(`클라이언트로부터 받은 메시지: ${message}`)
+    const parsedMessage = JSON.parse(message)
+    parsedMessage.isMine = false
+
     // 접속한 모든 클라이언트 들에게 메시지를 전송
     server.clients.forEach((client) => {
-      client.send(message.toString())
+      // 메시지를 보낸 클라이언트가 아닌 경우에만 메시지를 전송
+      if (client !== socket) {
+        client.send(JSON.stringify(parsedMessage))
+      }
+      else {
+        parsedMessage.isMine = true
+        client.send(JSON.stringify(parsedMessage))
+      }
     })
-
-    // socket.send(message)
   })
 
   // 클라이언트와의 연결이 종료되었을 때의 이벤트 리스너
@@ -26,4 +34,4 @@ server.on('connection', (socket) => {
   socket.send('서버로부터 메시지: 연결되었습니다.')
 })
 
-console.log('WebSocket 서버가 8080포트에서 실행 중입니다.')
+console.log('WebSocket 서버가 8079포트에서 실행 중입니다.')
--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -1,60 +1,42 @@
-<script setup>
-const chatText = ref('')
-const userName = ref('')
-const enterUser = ref(false)
-const msgList = ref([])
-let socket
-
-const sendMsg = () => {
-  if (!chatText.value)
-    return
-  socket.send(`${userName.value} : ${chatText.value}`)
-  chatText.value = ''
-}
-const setUser = () => {
-  if (!userName.value)
-    return
-  enterUser.value = true
-  socket = new WebSocket('ws://localhost:3333/chat')
-  socket.addEventListener('message', (event) => {
-    msgList.value.push(event.data)
-  })
-}
-
-onBeforeUnmount(() => {
-  if (socket)
-    socket.close()
-})
-</script>
-
 <template>
-  <div class="w-256 mx-auto">
-    <div class="text-2xl font-bold">
-      Chat
-    </div>
-    <div v-if="!enterUser" class="w-full flex items-center gap-4">
-      <p class="w-34">
-        User 이름
-      </p>
-      <el-input v-model="userName" placeholder="챗에 사용할 유저 이름을 입력해 주세요" @keyup.enter="setUser" />
-      <el-button @click="setUser">
-        보내기
-      </el-button>
-    </div>
-    <div v-else class="w-full flex items-center gap-4">
-      <el-input v-model="chatText" placeholder="Please input" @keyup.enter="sendMsg" />
-      <el-button @click="sendMsg">
-        보내기
-      </el-button>
-    </div>
-    <div class="text-left">
-      <p v-for="(text, idx) in msgList" :key="`msg-${idx}`">
-        {{ text }}
-      </p>
-    </div>
+  <div class="chat">
+    <el-container direction="vertical" class="chat-container">
+      <el-header class="chat-header">
+        <h1>OPEN TALK</h1>
+      </el-header>
+      <el-main>
+        <ChatTalkWindow />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <style scoped>
+.chat {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.chat-container {
+  height: auto;
+  max-height: 90%;
+  width: 70%;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.chat-header {
+  background-color: #409eff;
+  color: white;
+  text-align: center;
+  padding: 15px 0;
+  font-size: 1.5em;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 </style>
+
+<route lang="yaml">
+meta:
+  layout: bare
+</route>
